87dbd5aea21c1f9b0e9567d92ae278da238225f1
Seongjko <rhtjdwns2345@gmail.com>
2025-07-05 20:00:04 +0900

    Cashier 생성자에서 현금 결제 초기화 및 변경된 cashier에 맞춰 vendingMachine upate

--- src/function/cashier/Cashier.java
+++ src/function/cashier/Cashier.java
@@ -3,11 +3,16 @@ import java.util.Map;
 import java.util.HashMap;
 
 import payment.Payment;
+import payment.CashPayment;
 
 public class Cashier {
     Map<String, Payment> paymentMethods;
 
-    public Cashier() {}
+    public Cashier() {
+        this.paymentMethods = new HashMap<>();
+        CashPayment cashPayment = new CashPayment();
+        this.paymentMethods.put(cashPayment.getPaymentType(), cashPayment);
+    }
 
     public void addPaymentMethod(Payment payment) {
         paymentMethods.put(payment.getPaymentType(), payment);
