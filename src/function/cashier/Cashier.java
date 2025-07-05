1863626254a4fb1734d3dbf06562b8926b941c98
Seongjko <rhtjdwns2345@gmail.com>
2025-07-05 19:23:28 +0900

    Cashier의 책임을 결제수단 관리 및 제공으로 한정함

--- src/function/cashier/Cashier.java
+++ src/function/cashier/Cashier.java
@@ -1,26 +1,21 @@
 package function.cashier;
+import java.util.Map;
+import java.util.HashMap;
 
-import storage.CashStorage;
+import payment.Payment;
 
 public class Cashier {
+    Map<String, Payment> paymentMethods;
 
-    private CashStorage storage;
+    public Cashier() {}
 
-    //현재 구조로는 확장을 하게 되면 코드가 바뀌어야 된다 -> 유연성 X
-    public Cashier(CashStorage storage) {
-        this.storage = storage;
+    public void addPaymentMethod(Payment payment) {
+        paymentMethods.put(payment.getPaymentType(), payment);
     }
 
-    //카드의 경우에는 잔돈이 필요가 없다
-    public int calcCharge(String inputCash, int totalPrice) {
-        return storage.calcCharge(inputCash, totalPrice);
+    public Payment getPaymentMethod(String type) {
+        return paymentMethods.get(type);
     }
 
-    public void checkCashAmount() {
-        storage.checkCashAmount();
-    }
 
-    public void addCash(int amount) {
-        storage.addCash(amount);
-    }
 }
