f9e2db200445e7038be478aa1f5359ec19a57e80
Seongjko <rhtjdwns2345@gmail.com>
2025-07-06 21:16:06 +0900

    purchase initCommand 메서드 내 유효성 검사 위치 변경

--- src/model/command/Purchase.java
+++ src/model/command/Purchase.java
@@ -33,7 +33,6 @@ public class Purchase {
     public void initCommand(String input) throws ManageModeException, WrongParametersException, IllegalArgumentException {
         String[] parsed = parseInput(input);
 
-        validate();
         
         if (parsed.length != 3) {
             if (parsed.length == 1 && parsed[0].equals("manage")) {
@@ -58,6 +57,7 @@ public class Purchase {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("Error: Invalid number format");
         }
+        validate();
     }
     
     // 유효성 검사 메서드
